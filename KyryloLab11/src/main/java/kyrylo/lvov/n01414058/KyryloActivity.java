6a8458e534d9f7c48009ceaa540ff4d1e20632df
kyrylolvov <kyrylo@arccompute.io>
2022-05-22 13:54:31 -0400

    ADDED: - Background color of MainActivity - UI of buttons - EditField in MainActivity

--- KyryloLab11/src/main/java/kyrylo/lvov/n01414058/KyryloActivity.java
+++ KyryloLab11/src/main/java/kyrylo/lvov/n01414058/KyryloActivity.java
@@ -10,6 +10,6 @@ public class KyryloActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lvov);
+        setContentView(R.layout.activity_main);
     }
 }
